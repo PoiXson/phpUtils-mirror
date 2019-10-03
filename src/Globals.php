b1e2adbc85c41f49c5e09244d55dafcc9389b2c1
lorenzop <lorenzo@poixson.com>
2019-10-03 07:12:10 -0400

    removed old code from Globals.php

--- src/Globals.php
+++ src/Globals.php
@@ -5,95 +5,25 @@
  * @license GPL-3
  * @author lorenzo at poixson.com
  * @link https://poixson.com/
- * /
+ */
+namespace pxn\phpUtils {
+	final class Globals {
+		public static function init() {}
+	}
+}
+namespace {
 
 use pxn\phpUtils\Defines;
 
 
 
-# dump()
-if (!\function_exists('dump')) {
-	function dump($var, $msg=NULL) {
-		return \pxn\phpUtils\dump($var, $msg);
-	}
-}
-
-# d()
-if (!\function_exists('d')) {
-	function d($var, $msg=NULL) {
-		return \pxn\phpUtils\d($var, $msg);
-	}
-}
-
-# dd()
-if (!\function_exists('dd')) {
-	function dd($var, $msg=NULL) {
-		return \pxn\phpUtils\dd($var, $msg);
-	}
-}
-
-# ExitNow()
-if (!\function_exists('ExitNow')) {
-	function ExitNow($code=1) {
-		return \pxn\phpUtils\ExitNow($code);
-	}
-}
-
-# fail()
-if (!\function_exists('fail')) {
-	function fail($msg=NULL, $exitcode=1, \Exception $e=NULL) {
-		return \pxn\phpUtils\fail($msg, $exitcode, $e);
-	}
-}
-
-# backtrace()
-if (!\function_exists('backtrace')) {
-	function backtrace() {
-		return \pxn\phpUtils\backtrace();
-	}
-}
-
 # debug()
 if (!\function_exists('debug')) {
-	function debug($debug=NULL, $msg=NULL) {
-		return \pxn\phpUtils\debug($debug, $msg);
+	function debug($debug=NULL, $desc=NULL) {
+		return \pxn\phpUtils\Debug::debug($debug, $desc);
 	}
 }
 
-# pxdb
-if (\class_exists('pxn\\pxdb\\dbPool')) {
-	if (!\function_exists('pxdb_configure')) {
-		function pxdb_configure(
-			$dbName,
-			$driver,
-			$host,
-			$port,
-			$u,
-			$p,
-			$database,
-			$prefix
-		) {
-			return \pxn\pxdb\dbPool::configure(
-				$dbName,
-				$driver,
-				$host,
-				$port,
-				$u,
-				$p,
-				$database,
-				$prefix
-			);
-		}
-	}
-}
 
-if (!\function_exists('register_app')) {
-	function register_app($classPath) {
-		if (!\class_exists($classPath)) {
-			fail("App class doesn't exist: $classPath",
-				Defines::EXIT_CODE_INTERNAL_ERROR);
-		}
-		return $classPath::register();
-	}
-}
-*/
+
+} // end namespace
