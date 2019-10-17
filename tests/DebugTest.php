ab5d9e3b4faefab6d8d8126aa05a8c17b0081d4e
lorenzop <lorenzo@poixson.com>
2019-10-17 19:25:05 -0400

    added DebugTest class

---
+++ tests/DebugTest.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * PoiXson phpUtils - Website Utilities Library
+ * @copyright 2004-2019
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link https://poixson.com/
+ */
+namespace pxn\phpUtils\tests;
+
+use pxn\phpUtils\Debug;
+
+
+/**
+ * @coversDefaultClass \pxn\phpUtils\Debug
+ */
+class DebugTest extends \PHPUnit\Framework\TestCase {
+
+
+
+	/**
+	 * @covers ::isDebug
+	 * @covers ::setDebug
+	 * @covers ::getDesc
+	 * @covers ::setDesc
+	 * @covers ::EnableDisable
+	 */
+	public function testGetSetDebug() {
+		$originalDebug = Debug::isDebug();
+		$originalDesc  = Debug::getDesc();
+		// enable debug
+		Debug::setDebug(TRUE, 'unit test');
+		$this->assertTrue(Debug::isDebug());
+		$this->assertEquals('unit test', Debug::getDesc());
+		// null
+		Debug::setDebug(NULL, 'null');
+		$this->assertTrue(Debug::isDebug());
+		$this->assertEquals('unit test', Debug::getDesc());
+		// disable debug
+		Debug::setDebug(FALSE, 'unit test');
+		$this->assertFalse(Debug::isDebug());
+		$this->assertEquals('unit test', Debug::getDesc());
+		// set desc
+		Debug::setDesc('setDesc() test');
+		$this->assertEquals('setDesc() test', Debug::getDesc());
+		// restore debug
+		Debug::setDebug($originalDebug, $originalDesc);
+		$this->assertEquals($originalDebug, Debug::isDebug());
+	}
+
+
+
+	/**
+	 * @covers ::debug
+	 * @covers ::desc
+	 * @covers ::EnableDisable
+	 */
+	public function testDebug() {
+		$originalDebug = Debug::debug();
+		$originalDesc  = Debug::desc();
+		// enable debug
+		Debug::debug(TRUE, 'unit test');
+		$this->assertTrue(Debug::debug());
+		$this->assertEquals('unit test', Debug::desc());
+		// disable debug
+		Debug::debug(FALSE, 'unit test');
+		$this->assertFalse(Debug::debug());
+		$this->assertEquals('unit test', Debug::desc());
+		// set desc
+		Debug::desc('desc test 1');
+		$this->assertEquals('desc test 1', Debug::desc('desc test 2'));
+		$this->assertEquals('desc test 2', Debug::desc());
+		// restore debug
+		Debug::debug($originalDebug, $originalDesc);
+		$this->assertEquals($originalDebug, Debug::debug());
+	}
+
+
+
+}
