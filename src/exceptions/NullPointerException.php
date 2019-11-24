90eecccb727e823e64c74e5a23b24f1afb44d7c3
lorenzop <lorenzo@poixson.com>
2019-11-24 12:22:38 -0500

    added NullPointerException class

---
+++ src/exceptions/NullPointerException.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * PoiXson phpUtils - PHP Utilities Library
+ * @copyright 2004-2019
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link https://poixson.com/
+ */
+namespace pxn\phpUtils\exceptions;
+
+
+class NullPointerException extends \RuntimeException {
+
+
+
+	public function __construct(string $msg=NULL) {
+		if (empty($msg)) {
+			parent::__construct();
+		} else {
+			parent::__construct("File not found: $msg");
+		}
+	}
+
+
+
+}
