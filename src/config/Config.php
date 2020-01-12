5846aa22568acaeec0f126e162c64efea11629f9
lorenzop <lorenzo@poixson.com>
2020-01-12 12:35:12 -0500

    renamed Load..() functions to load..() in Config class

--- src/config/Config.php
+++ src/config/Config.php
@@ -18,7 +18,7 @@ class Config extends ConfigDAO {
 
 	public function __construct(string $file=NULL) {
 		if ($file !== NULL) {
-			$this->LoadFile($file);
+			$this->loadFile($file);
 		}
 		parent::__construct();
 	}
@@ -37,7 +37,7 @@ class Config extends ConfigDAO {
 
 
 
-	public function LoadFile(string $file=NULL): void {
+	public function loadFile(string $file=NULL): void {
 		if (!empty($file)) {
 			$file = (string) $file;
 			if (!empty($file)) {
@@ -67,9 +67,9 @@ class Config extends ConfigDAO {
 			}
 			$ext = \mb_substr($this->file, $pos);
 			if ($ext == '.json') {
-				$this->LoadStringJson($data);
+				$this->loadStringJson($data);
 			} elseif ($ext == '.yml') {
-				$this->LoadStringYaml($data);
+				$this->loadStringYaml($data);
 			} else {
 				throw new \RuntimeException("Unknown config file type: $ext");
 			}
@@ -77,19 +77,19 @@ class Config extends ConfigDAO {
 		}
 		$this->mtime = \filemtime($this->file);
 	}
-	public function LoadStringJson(string $data): void {
+	public function loadStringJson(string $data): void {
 		if (empty($data)) return;
 		$json = \json_decode($data, TRUE, \JSON_OBJECT_AS_ARRAY | \JSON_THROW_ON_ERROR);
 		if ($json === NULL) {
-			throw new \RuntimeException("Failed to parse config file");
+			throw new \RuntimeException("Failed to parse config file: {$this->file}");
 		}
 		$this->data = $json;
 	}
-	public function LoadStringYaml(string $data): void {
+	public function loadStringYaml(string $data): void {
 		if (empty($data)) return;
 		$yaml = \yaml_parse($data);
 		if ($yaml === NULL) {
-			throw new \RuntimeException("Failed to parse config file");
+			throw new \RuntimeException("Failed to parse config file: {$this->file}");
 		}
 		$this->data = $yaml;
 	}
