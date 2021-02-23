fe9f935faf5dddddf758035d0deba43bb970bb80
lorenzop <lorenzo@poixson.com>
2021-02-23 01:29:04 -0500

    updated test for pxnDefines class

--- tests/test_pxnDefines.php
+++ tests/test_pxnDefines.php
@@ -5,48 +5,57 @@
  * @license GPL-3
  * @author lorenzo at poixson.com
  * @link https://poixson.com/
- * /
+ */
 namespace pxn\phpUtils\tests;
 
-use pxn\phpUtils\Defines;
+use pxn\phpUtils\pxnDefines as xDef;
 
 
-class DefinesTest extends \PHPUnit\Framework\TestCase {
+/**
+ * @coversDefaultClass \pxn\phpUtils\pxnDefines
+ */
+class test_pxnDefines extends \PHPUnit\Framework\TestCase {
 
 
 
-	public function testValues() {
+	/**
+	 * @covers ::init
+	 */
+	public function test_Values(): void {
+		xDef::init();
+		// phpUtils version
+		$this->assertNotEmpty(xDef::phpUtilsVersion);
+		$this->assertTrue(\str_starts_with(haystack: xDef::phpUtilsVersion, needle: "0."));
 		// php version
-		$this->assertGreaterThanOrEqual( 50600, Defines::PHP_MIN_VERSION );
-		$this->assertLessThanOrEqual   ( 70100, Defines::PHP_MIN_VERSION );
+		$this->assertGreaterThanOrEqual(expected: 80000, actual: xDef::PHP_MIN_VERSION);
+		$this->assertLessThan(          expected: 90000, actual: xDef::PHP_MIN_VERSION);
 		// common characters
-		$this->assertEquals( Defines::DIR_SEP, \DIRECTORY_SEPARATOR );
-		$this->assertEquals( Defines::NEWLINE, Defines::EOL         );
-		$this->assertEquals( Defines::TAB,     "\t"                 );
-		$this->assertEquals( Defines::QUOTE_S, '\''                 );
-		$this->assertEquals( Defines::QUOTE_D, "\""                 );
-		$this->assertEquals( Defines::ACCENT,  '`'                  );
+		$this->assertEquals(expected: xDef::DIR_SEP, actual: \DIRECTORY_SEPARATOR);
+		$this->assertEquals(expected: xDef::NEWLINE, actual: xDef::EOL           );
+		$this->assertEquals(expected: xDef::TAB,     actual: "\t"                );
+		$this->assertEquals(expected: xDef::QUOTE_S, actual: '\''                );
+		$this->assertEquals(expected: xDef::QUOTE_D, actual: "\""                );
+		$this->assertEquals(expected: xDef::ACCENT,  actual: '`'                 );
 		// common numbers
-		$this->assertEquals( Defines::INT_MAX,  2147483647 );
-		$this->assertEquals( Defines::INT_MIN, -2147483648 );
-		$this->assertEquals( Defines::NET_PORT_MAX,  65535 );
+		$this->assertGreaterThanOrEqual(expected: 2147483647, actual: xDef::INT_MAX);
+		$this->assertLessThanOrEqual(   expected:-2147483648, actual: xDef::INT_MIN);
+		$this->assertEquals(            expected: xDef::NET_PORT_MAX, actual: 65535);
 		// number of seconds
-		$this->assertEquals( Defines::S_MS,       0.001 );
-		$this->assertEquals( Defines::S_SECOND,       1 );
-		$this->assertEquals( Defines::S_MINUTE,      60 );
-		$this->assertEquals( Defines::S_HOUR,      3600 );
-		$this->assertEquals( Defines::S_DAY,      86400 );
-		$this->assertEquals( Defines::S_WEEK,    604800 );
-		$this->assertEquals( Defines::S_MONTH,  2592000 );
-		$this->assertEquals( Defines::S_YEAR,  31536000 );
+		$this->assertEquals(expected: xDef::S_MS,     actual:               0.001);
+		$this->assertEquals(expected: xDef::S_SECOND, actual:                   1);
+		$this->assertEquals(expected: xDef::S_MINUTE, actual:                  60);
+		$this->assertEquals(expected: xDef::S_HOUR,   actual:                3600);
+		$this->assertEquals(expected: xDef::S_DAY,    actual:               86400);
+		$this->assertEquals(expected: xDef::S_WEEK,   actual:              604800);
+		$this->assertEquals(expected: xDef::S_MONTH,  actual:             2592000);
+		$this->assertEquals(expected: xDef::S_YEAR,   actual:            31536000);
 		// number of bytes
-		$this->assertEquals( Defines::KB,                1024 );
-		$this->assertEquals( Defines::MB,           1024*1024 );
-		$this->assertEquals( Defines::GB,      1024*1024*1024 );
-		$this->assertEquals( Defines::TB, 1024*1024*1024*1024 );
+		$this->assertEquals(expected: xDef::KB,       actual:                1024);
+		$this->assertEquals(expected: xDef::MB,       actual:           1024*1024);
+		$this->assertEquals(expected: xDef::GB,       actual:      1024*1024*1024);
+		$this->assertEquals(expected: xDef::TB,       actual: 1024*1024*1024*1024);
 	}
 
 
 
 }
-*/
