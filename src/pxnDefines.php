5336da3c10fa872d771dc79f46eae65b02a80770
lorenzop <lorenzo@poixson.com>
2021-02-23 03:17:55 -0500

    cleaned up pxnDefines class

--- src/pxnDefines.php
+++ src/pxnDefines.php
@@ -10,6 +10,7 @@ namespace pxn\phpUtils;
 
 
 final class pxnDefines {
+	/** @codeCoverageIgnore */
 	private final function __construct() {}
 	public static function init(): void {}
 
@@ -17,15 +18,16 @@ final class pxnDefines {
 	const phpUtilsVersion = \pxn\phpUtils\Version::phpUtilsVersion;
 
 
-	const PHP_MIN_VERSION     = 70300;
-	const PHP_MIN_VERSION_STR = '7.3';
+	const PHP_MIN_VERSION     = 80000;
+	const PHP_MIN_VERSION_STR = '8.0';
+
 
 	const DEBUG_COOKIE = 'pxn_debug';
 
 
-//	const KEY_CFG_ANSI_COLOR_ENABLED      = 'ansi color enabled';
-//	const KEY_CFG_ALLOW_SHORT_FLAG_VALUES = 'allow short flag values';
-//	const KEY_CFG_DISPLAY_MODE            = 'display mode';
+	const INT_MAX      = PHP_INT_MAX;
+	const INT_MIN      = PHP_INT_MIN;
+	const NET_PORT_MAX = 65535;
 
 
 	const DIR_SEP      = \DIRECTORY_SEPARATOR;
@@ -39,9 +41,6 @@ final class pxnDefines {
 	const D_QUOTE      = self::QUOTE_D;
 	const ACCENT       = '`';
 
-	const INT_MAX      = 2147483647;
-	const INT_MIN      =-2147483648;
-	const NET_PORT_MAX =      65535;
 
 	// number of seconds
 	const S_MS      =    0.001;
@@ -53,24 +52,12 @@ final class pxnDefines {
 	const S_MONTH   =  2592000;
 	const S_YEAR    = 31536000;
 
+
 	// number of bytes
 	const KB =          1024;
 	const MB =       1048576;
 	const GB =    1073741824;
 	const TB = 1099511627776;
-
-	const EXIT_CODE_OK               =   0;
-	const EXIT_CODE_GENERAL          =   1;
-	const EXIT_CODE_HELP             =   2;
-	const EXIT_CODE_INTERNAL_ERROR   =   4;
-	const EXIT_CODE_NOPERM           =   8;
-//	const EXIT_CODE_USAGE_ERROR      =  64;
-//	const EXIT_CODE_INVALID_FORMAT   =  65;
-//	const EXIT_CODE_UNAVAILABLE      =  69;
-//	const EXIT_CODE_IO_ERROR         =  74;
-//	const EXIT_CODE_CONFIG_ERROR     =  78;
-//	const EXIT_CODE_INVALID_COMMAND  = 127;
-//	const EXIT_CODE_INVALID_ARGUMENT = 128;
 
 
 }
