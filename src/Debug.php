88c59ffb9f07f794dec7bd0c186f9c4b21088ed5
lorenzop <lorenzo@poixson.com>
2021-03-21 05:01:15 -0400

    cleaned up Debug.php and Globals.php files

--- src/Debug.php
+++ src/Debug.php
@@ -5,132 +5,57 @@
  * @license GPL-3
  * @author lorenzo at poixson.com
  * @link https://poixson.com/
- * /
+ */
 namespace pxn\phpUtils;
 
 
 final class Debug {
 	private function __construct() {}
 
-	private static $inited = FALSE;
+	private static bool $inited = false;
 
-	protected static $debug = NULL;
-	protected static $desc  = NULL;
+	private static ?bool $debug = null;
+	private static ?string $desc = null;
 
 
 
-	/ **
-	 * @codeCoverageIgnore
-	 * /
+//TODO: enable by remote ip
+//TODO: disable in production
 	public static function init(): void {
 		if (self::$inited) return;
-		self::$inited = TRUE;
+		self::$inited = true;
 		// by define
 		if (\defined('DEBUG')) {
-			self::setDebug(\DEBUG, 'by define');
-		}
+			self::debug(true, 'by define');
+		} else
 		if (\defined('pxn\\phpUtils\\DEBUG')) {
-			self::setDebug(\pxn\phpUtils\DEBUG, 'by namespace define');
+			self::debug(true, 'by phpUtils define');
 		}
-		// by file
-		{
-			$finder = new FileFinder();
-			$finder->searchPath(Paths::entry(), 2);
-//TODO
-//$common = Strings::CommonPath(
-//	$entry
-//	$base
-//);
-//$paths = [
-//	$entry,
-//	$base,
-//	$common,
-//	$common."/.."
-//];
-			$finder->searchFiles(
-				'.debug',
-				'debug',
-				'DEBUG',
-			);
-			$found = $finder->find();
-			if (!empty($found)) {
-				self::setDebug(TRUE, 'by file');
-			}
-			unset($finder, $found);
-		}
-//TODO: disable in production
-/ *
-		// by url
-		{
-			$val = GeneralUtils::getVar('debug', 'bool');
-			if ($val !== NULL) {
-				// set cookie
-				\setcookie(
-					Defines::DEBUG_COOKIE,
-					($val ? '1', '0'),
-					0
-				);
-				self::setDebug($val, 'by url');
-			} else {
-				$val = GeneralUtils::getVar(Defines::DEBUG_COOKIE, 'bool', 'cookie');
-				if ($val === TRUE) {
-					self::setDebug($val, 'by cookie');
+		// .debug file
+		$paths = [
+			Paths::pwd().'/',
+			Paths::pwd().'/../',
+		];
+		$files = [
+			'debug',
+			'.debug',
+		];
+		foreach ($paths as $path) {
+			foreach ($files as $file) {
+				if (\is_file($path.$file)) {
+					self::debug(true, "by $file file");
+					break 2;
 				}
 			}
-			unset($val);
 		}
-* /
-		// default off
-		if (self::$debug === NULL) {
-			self::$desc = 'default';
-			self::EnableDisable(FALSE);
-		}
-	}
-
-
-
-	public static function debug(?bool $debug=NULL, ?string $desc=NULL): bool {
-		$last = self::isDebug();
-		if ($debug !== NULL) {
-			self::setDebug($debug, $desc);
-		}
-		return $last;
-	}
-	public static function isDebug(): bool {
-		return (self::$debug === TRUE);
-	}
-	public static function setDebug(bool $debug, ?string $desc=NULL): void {
-		if (!self::$inited) self::init();
-		if ($debug === NULL) return;
-		$last = self::$debug;
-		self::$debug = ($debug !== FALSE);
-		self::$desc = $desc;
-		// set change
-		if (self::$debug != $last) {
-			self::EnableDisable(self::$debug);
-		}
-	}
-
-
-
-	public static function desc(?string $desc=NULL): string {
-		$last = self::getDesc();
-		if ($desc !== NULL) {
-			self::setDesc($desc);
-		}
-		return $last;
-	}
-	public static function getDesc(): string {
-		return self::$desc;
-	}
-	public static function setDesc(string $desc): void {
-		self::$desc = $desc;
 	}
 
 
 
 	private static function EnableDisable(bool $debug): void {
-		$isShell = SystemUtils::isShell();
+//TODO
+//		$isShell = SystemUtils::isShell();
+$isShell = false;
 		\error_reporting(\E_ALL);
 		\ini_set('display_errors', $debug   ? 'On' : 'Off');
 		\ini_set('html_errors',    $isShell ? 'Off' : 'On');
@@ -141,5 +66,41 @@ final class Debug {
 
 
 
+	public static function debug(?bool $enable=null, ?string $desc=null): bool {
+		if ($enable !== null) {
+			if ($enable !== self::$debug) {
+				self::$debug = $enable;
+				self::EnableDisable(self::$enable);
+				self::addDesc($desc);
+			}
+		}
+		// default
+		if (self::$debug === null)
+			return false;
+		return self::$debug;
+	}
+
+
+
+	public static function desc(?string $desc=null): ?string {
+		if (!empty($desc)) {
+			if (!empty(self::$desc))
+				self::$desc .= '; ';
+			self::$desc .= $desc;
+		}
+		if (self::debug())
+			return (empty(self::$desc) ? '' : self::$desc);
+		return null;
+	}
+
+
+
+	public static function dd($data=null): void {
+		if ($data !== null)
+			\print_r($data);
+		exit(1);
+	}
+
+
+
 }
-*/
--- src/Globals.php
+++ src/Globals.php
@@ -5,26 +5,36 @@
  * @license GPL-3
  * @author lorenzo at poixson.com
  * @link https://poixson.com/
- * /
+ */
 namespace pxn\phpUtils {
+
+
+
 	final class Globals {
 		public static function init(): void {}
 	}
+
+
+
 }
 namespace {
 
-use pxn\phpUtils\Defines;
 
 
-
-# debug()
-if (!\function_exists('debug')) {
-	function debug(?bool $debug=NULL, ?string $desc=NULL): bool {
-		return \pxn\phpUtils\Debug::debug($debug, $desc);
+	# debug()
+	if (!\function_exists('debug')) {
+		function debug(?bool $debug=NULL, ?string $desc=NULL): bool {
+			return \pxn\phpUtils\Debug::debug($debug, $desc);
+		}
 	}
+
+	# dd()
+	if (!\function_exists('dd')) {
+		function dd($data=null): void {
+			\pxn\phpUtils\Debug\dd($data);
+		}
+	}
+
+
+
 }
-
-
-
-} // end namespace
-*/
