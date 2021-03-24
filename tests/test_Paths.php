cb756e8cf3b7f123a4e2fc677cb3a9cfae46f7ca
lorenzop <lorenzo@poixson.com>
2021-03-24 19:21:33 -0400

    added more tests for Paths.php

--- tests/test_Paths.php
+++ tests/test_Paths.php
@@ -19,10 +19,12 @@ class test_Paths extends \PHPUnit\Framework\TestCase {
 
 
 	/**
-	 * @covers ::pwd()
-	 * @covers ::entry()
-	 * @covers ::get()
-	 * @covers ::all()
+	 * @covers ::init
+	 * @covers ::pwd
+	 * @covers ::entry
+	 * @covers ::get
+	 * @covers ::set
+	 * @covers ::getAll
 	 */
 	public function test_paths(): void {
 		$all = [];
@@ -39,16 +41,53 @@ class test_Paths extends \PHPUnit\Framework\TestCase {
 		$this->assertNotEmpty($path);
 		$this->assertTrue(\str_starts_with(haystack: $path, needle: '/'));
 		$this->assertEquals(expected: $path, actual: Paths::get('entry'));
+		// set custom path
+		$all['home'] = '/home';
+		Paths::set(key: 'home', path: '/home');
+		$this->assertEquals(expected: '/home', actual: Paths::get('home'));
+		// set with {pwd}
+		$all['abc'] = Paths::pwd().'/abc';
+		Paths::set(key: 'abc', path: '{pwd}/abc');
+		$this->assertEquals(expected: $all['abc'], actual: Paths::get('abc'));
+		// set with {entry}
+		$all['abcd'] = Paths::entry().'/abcd';
+		Paths::set(key: 'abcd', path: '{entry}/abcd');
+		$this->assertEquals(expected: $all['abcd'], actual: Paths::get('abcd'));
+		// set with {home}
+		$all['docs'] = '/home/user/docs';
+		Paths::set(key: 'docs', path: '{home}/user/docs');
+		$this->assertEquals(expected: $all['docs'], actual: Paths::get('docs'));
 		// all paths
-		$this->assertEquals(expected: $all, actual: Paths::all());
+		$this->assertEquals(expected: $all, actual: Paths::getAll());
 	}
+
 	/**
-	 * @covers ::get()
+	 * @covers ::assertPathSet
+	 */
+	public function test_assertPathSet(): void {
+		Paths::assertPathSet('pwd');
+		Paths::assertPathSet('entry');
+		$this->expectException(\RuntimeException::class);
+		$this->expectExceptionMessage('Path not set: test');
+		Paths::assertPathSet('test');
+	}
+
+	/**
+	 * @covers ::get
 	 */
 	public function test_path_unknown(): void {
 		$this->expectException(\RuntimeException::class);
 		$this->expectExceptionMessage('Path not set: unknown');
 		Paths::get('unknown');
+	}
+
+	/**
+	 * @covers ::set
+	 */
+	public function test_path_tag_unknown(): void {
+		$this->expectException(\RuntimeException::class);
+		$this->expectExceptionMessage('Unknown path tag: unknown');
+		Paths::set(key: 'fail', path: '{unknown}/fail');
 	}
 
 
