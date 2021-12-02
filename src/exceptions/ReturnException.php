c025164e3fbfe0fb3bd8e489d8143c703673220e
lorenzop <lorenzo@poixson.com>
2021-12-02 16:23:49 -0500

    added ReturnException class

---
+++ src/exceptions/ReturnException.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types = 1);
+/*
+ * PoiXson phpUtils - PHP Utilities Library
+ * @copyright 2004-2021
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link https://poixson.com/
+ */
+namespace pxn\phpUtils\exceptions;
+
+
+class ReturnException extends \RuntimeException {
+
+	public $result = null;
+
+
+
+	public function __construct($result=null) {
+		$this->result = $result;
+		if (empty($result)) {
+			parent::__construct();
+		} else {
+			parent::__construct("Return: $result");
+		}
+	}
+
+
+
+	public function getResult() {
+		return $this->result;
+	}
+
+
+
+}
