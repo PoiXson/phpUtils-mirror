705c14c7ef62430990ed491e6a4d310627b05421
lorenzop <lorenzo@poixson.com>
2022-08-12 06:23:40 -0400

    cleaned up xApp class

--- src/app/xApp.php
+++ src/app/xApp.php
@@ -11,73 +11,16 @@ namespace pxn\phpUtils\app;
 
 abstract class xApp {
 
-}
-/*
-//use pxn\phpUtils\utils\StringUtils;
-//use pxn\phpUtils\Paths;
 
-//	protected string $appName;
-//	protected string $appNS;
-//	protected string $version;
 
 	public function __construct() {
-		$this->check_run_mode();
-		// find class name and namespace
-		{
-			$clss = StringUtils::Trim(\get_called_class(), '\\');
-			$pos = \mb_strrpos($clss, '\\');
-			if ($pos === false || $pos <= 3) {
-				$this->appName = $clss;
-				$this->appNS   = '';
-			} else {
-				$this->appName = StringUtils::Trim(\mb_substr($clss, $pos),    '\\');
-				$this->appNS   = StringUtils::Trim(\mb_substr($clss, 0, $pos), '\\');
-			}
-			if (empty($this->appName))
-				throw new \RuntimeException('Failed to find app name');
-		}
-		// app version
-		{
-			$clss = "\\{$this->appNS}\\Version";
-			$cnst = "{$clss}::version";
-			if (!\class_exists($clss) || !\defined($cnst))
-				throw new \RuntimeException("Class not found: $clss");
-			$this->version = \constant($cnst);
-			if (empty($this->version))
-				throw new \RuntimeException('App version not found');
-		}
-		// paths
-		$this->load_paths();
-		// init app
-		$this->init();
 	}
 
 
 
-	protected function init(): void {
-	}
-	protected function load_paths(): void {
-		Paths::init();
+	public function run(): void {
 	}
 
 
 
-	protected abstract function check_run_mode(): void;
-
-	public abstract function run(): void;
-
-
-
-	public function getName(): string {
-		return $this->appName;
-	}
-	public function getNamespace(): string {
-		return $this->appNS;
-	}
-
-
-
-	public function getVersion(): string {
-		return $this->version;
-	}
-*/
+}
