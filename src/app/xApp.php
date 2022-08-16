8aa15521d5a65433dfa44d7aee09075918e3f45f
lorenzop <lorenzo@poixson.com>
2022-08-16 16:56:34 -0400

    added load_paths() function to xApp class - using a simple array

--- src/app/xApp.php
+++ src/app/xApp.php
@@ -11,9 +11,28 @@ namespace pxn\phpUtils\app;
 
 abstract class xApp {
 
+	public array $paths = [];
+
 
 
 	public function __construct() {
+		$this->load_paths();
+	}
+
+
+
+	public function load_paths(): void {
+		$entry = $_SERVER['DOCUMENT_ROOT'];
+		$root = (
+			\str_ends_with($entry, '/public')
+			? \mb_substr($entry, 0, -7)
+			: $entry
+		);
+		$this->paths['root']  = $root;
+		$this->paths['entry'] = $entry;
+		$this->paths['html']   = "$root/html";
+		$this->paths['static'] = "$entry/static";
+		$this->paths['data']   = "$root/data";
 	}
 
 
