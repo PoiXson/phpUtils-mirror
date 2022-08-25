5659cf2903692510c02a1cba0f8e8c553025c618
lorenzop <lorenzo@poixson.com>
2022-08-25 16:13:52 -0400

    updated tests for PathUtils class

--- tests/utils/test_PathUtils.php
+++ tests/utils/test_PathUtils.php
@@ -19,47 +19,25 @@ class test_PathUtils extends \PHPUnit\Framework\TestCase {
 
 
 	/**
-	 * @covers ::get_filename
+	 * @covers ::FileName
 	 */
-	public function test_get_filename(): void {
-		$this->assertEquals(expected: '',         actual: PathUtils::get_filename('')             );
-		$this->assertEquals(expected: 'hostname', actual: PathUtils::get_filename('/etc/hostname'));
-		$this->assertEquals(expected: 'hostname', actual: PathUtils::get_filename('hostname')     );
+	public function test_FileName(): void {
+		$this->assertEquals(expected: '',         actual: PathUtils::FileName(''             ));
+		$this->assertEquals(expected: 'hostname', actual: PathUtils::FileName('/etc/hostname'));
+		$this->assertEquals(expected: 'hostname', actual: PathUtils::FileName('hostname'     ));
 	}
 
 
 
 	/**
-	 * @covers ::build_path
+	 * @covers ::TrimPath
 	 */
-	public function test_build_path(): void {
-		$this->assertEquals(expected: '',                    actual: PathUtils::build_path()                                );
-		$this->assertEquals(expected: 'home',                actual: PathUtils::build_path('', 'home', '')                  );
-		$this->assertEquals(expected: 'home/user/Desktop',   actual: PathUtils::build_path('home', 'user', 'Desktop')       );
-		$this->assertEquals(expected: '/home/user/Desktop',  actual: PathUtils::build_path('/', 'home', 'user', 'Desktop')  );
-		$this->assertEquals(expected: '/home/user/Desktop/', actual: PathUtils::build_path('/home', 'user', 'Desktop', '/') );
-		$this->assertEquals(expected: '/home/user/Desktop/', actual: PathUtils::build_path('/home/', '/user/', '/Desktop/') );
-	}
-
-
-
-	/**
-	 * @covers ::common_path
-	 */
-	public function test_common_path(): void {
-		$this->assertEquals(expected: '',           actual: PathUtils::common_path('', '')                                       );
-		$this->assertEquals(expected: '/home/user', actual: PathUtils::common_path('/home/user/Desktop', '/home/user/Documents') );
-		$this->assertEquals(expected: '/home/user', actual: PathUtils::common_path('/home/user/',        '/home/user/Documents/'));
-		$this->assertEquals(expected: '/',          actual: PathUtils::common_path('/usr/bin',           '/etc/profile.d')       );
-	}
-
-
-
-	/**
-	 * @covers ::resolve_symlinks
-	 */
-	public function test_resolve_symlinks(): void {
-		$this->assertEquals(expected: 'usr/bin', actual: PathUtils::resolve_symlinks('/bin'));
+	public function test_TrimPath(): void {
+		$path = '/etc/nginx/conf.d/website.conf';
+		$this->assertEquals(expected: false,                 actual: PathUtils::TrimPath('',    '/etc'              ));
+		$this->assertEquals(expected: false,                 actual: PathUtils::TrimPath($path, '/var/www'          ));
+		$this->assertEquals(expected: 'conf.d/website.conf', actual: PathUtils::TrimPath($path, '/etc/nginx'        ));
+		$this->assertEquals(expected: 'website.conf',        actual: PathUtils::TrimPath($path, '/etc/nginx/conf.d/'));
 	}
 
 
