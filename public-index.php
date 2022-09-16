493b464ef6240cfc3e4b8673bbdbe4bfa935f9f0
lorenzop <lorenzo@poixson.com>
2022-09-16 03:11:27 -0400

    added more newline spaces in public-index.php

--- public-index.php
+++ public-index.php
@@ -8,7 +8,7 @@ if (\is_file(__DIR__.'/vendor/autoload.php')) {
 if (\is_file(__DIR__.'/../vendor/autoload.php')) {
 	$loader = require(__DIR__.'/../vendor/autoload.php');
 }
-if ($loader == null) { echo "Failed to detect autoload.php\n"; exit(1); }
+if ($loader == null) { echo "Failed to detect autoload.php\n\n"; exit(1); }
 
 // find app class
 $app_class = null;
@@ -18,16 +18,16 @@ if (\file_exists(__DIR__.'/.xapp')) {
 if (\file_exists(__DIR__.'/../.xapp')) {
 	$app_class = \file_get_contents(__DIR__.'/../.xapp');
 }
-if (empty($app_class)) { echo "Failed to detect app class\n"; exit(1); }
+if (empty($app_class)) { echo "Failed to detect app class\n\n"; exit(1); }
 $app_class = \explode("\n", $app_class, 2);
 $app_class = \trim(\reset($app_class));
 if (\str_ends_with($app_class, '\\'))
 	$app_class = \mb_substr($app_class, 0, -1);
-if (empty($app_class)) { echo "Invalid app class\n"; exit(1); }
+if (empty($app_class)) { echo "Invalid app class\n\n"; exit(1); }
 if (!\str_ends_with($app_class, '\\Website')
 &&  !\str_ends_with($app_class, '\\ShellApp'))
 	throw new \RuntimeException("Invalid app class: $app_class");
-if (!\class_exists($app_class)) { echo "App class not found: $app_class\n"; exit(1); }
+if (!\class_exists($app_class)) { echo "App class not found: $app_class\n\n"; exit(1); }
 
 // load app
 $app = new $app_class($loader);
