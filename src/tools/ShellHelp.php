5ba9f582b17b17339b4b7d4df55925bc16771c9b
lorenzop <lorenzo@poixson.com>
2024-07-18 04:19:48 -0400

    more cleanup

--- src/tools/ShellHelp.php
+++ src/tools/ShellHelp.php
@@ -5,7 +5,7 @@
  * @license AGPL-3
  * @author lorenzo at poixson.com
  * @link https://poixson.com/
- * /
+ */
 namespace pxn\phpUtils\tools;
 
 use \pxn\phpUtils\utils\ShellUtils;
@@ -16,14 +16,14 @@ class ShellHelp {
 	const HELP_WIDTH       = 80;
 	const MIN_FLAG_SPACING = 11;
 
-	protected $name = null;
-	protected $msg  = null;
-	protected $appendUsage = [];
+	protected ?string $name = null;
+	protected ?string $msg  = null;
+	protected array $appendUsage = [];
 
-	protected $command  = null;
-	protected $commands = [];
+	protected ?string $command  = null;
+	protected array   $commands = [];
 
-	protected $flags     = [
+	protected array $flags = [
 		'pre' => [],
 		'mid' => [],
 		'pst' => []
@@ -31,56 +31,39 @@ class ShellHelp {
 
 
 
-	public function __construct($command=null) {
+	public function __construct(?string $command=null) {
 		$this->setCommand($command);
 	}
 
 
 
-	public function setSelfName($name) {
-		$this->name =
-			empty($name)
-			? null
-			: (string) $name;
+	public function setSelfName(?string $name): self {
+		$this->name = (empty($name) ? null : (string) $name);
 		return $this;
 	}
-	public function appendSelfName($name) {
-		$this->name =
-			\implode([
-				$this->name,
-				$name
-			],' ');
+	public function appendSelfName(string $name): void {
+		$this->name = \implode([ $this->name, $name ],' ');
 	}
-	public function getSelfName() {
-		if (empty($this->name)) {
+	public function getSelfName(): string {
+		if (empty($this->name))
 			return \basename($_SERVER['PHP_SELF']);
-		}
 		return (string) $this->name;
 	}
 
 
 
-	public function setMessage($msg=null) {
-		if (empty($msg)) {
-			$this->msg = null;
-		} else
-		if (\is_array($msg)) {
-			$this->msg = \implode("\n", $msg);
-		} else {
-			$this->msg = (string) $msg;
-		}
+	public function setMessage(?string $msg=null): self {
+		if (empty($msg))          $this->msg = null;
+		else if (\is_array($msg)) $this->msg = \implode("\n", $msg);
+		else                      $this->msg = $msg;
 		return $this;
 	}
 
 
 
-	public function appendUsage($msg) {
-		$msg = (string) $msg;
+	public function appendUsage(string $msg): void {
 		if (\is_array($msg)) {
-			$this->appendUsage = \array_merge(
-				$this->appendUsage,
-				$msg
-			);
+			$this->appendUsage = \array_merge($this->appendUsage, $msg);
 		} else
 		if (!empty($msg)) {
 			$this->appendUsage[] = $msg;
@@ -89,34 +72,23 @@ class ShellHelp {
 
 
 
-	public function setCommand($command) {
-		$this->command =
-			empty($command)
-			? null
-			: (string) $command;
+	public function setCommand(string $command): self {
+		$this->command = (empty($command) ? null : (string) $command;
 		return $this;
 	}
-	public function addCommands($commands) {
-		$this->commands = \array_merge(
-			$this->commands,
-			$commands
-		);
+	public function addCommands(array $commands): self {
+		$this->commands = \array_merge($this->commands, $commands);
 		return $this;
 	}
 
 
 
-	public function addFlags($flags, $position=null) {
-		$position = (string) $position;
+	public function addFlags(array $flags, ?string $position=null): self {
 		$position = \mb_strtolower($position);
-		if ($position == 'post') {
+		if ($position == 'post')
 			$position = 'pst';
-		}
 		if (isset($this->flags[$position])) {
-			$this->flags[$position] = \array_merge(
-				$this->flags[$position],
-				$flags
-			);
+			$this->flags[$position] = \array_merge($this->flags[$position], $flags);
 		} else {
 			$this->flags[$position] = $flags;
 		}
@@ -125,58 +97,51 @@ class ShellHelp {
 
 
 
-	public function Display() {
+	public function Display(): void {
 		echo "\n";
 		$this->Display_Usage();
 		$this->Display_Commands();
 		$this->Display_Flags();
 	}
-	public function Display_Usage() {
+	public function Display_Usage(): void {
 		$usage = [];
 		$usage[] = $this->getSelfName();
 		if (empty($this->command)) {
-			if (!\is_array($this->commands) || \count($this->commands) > 0) {
+			if (!\is_array($this->commands) || \count($this->commands) > 0)
 				$usage[] = '<command>';
-			}
 		} else {
 			$usage[] = (string) $this->command;
 		}
 		$usage[] = '[flags]';
-		if (\is_array($this->appendUsage) && \count($this->appendUsage) > 0) {
-			$usage = \array_merge(
-				$usage,
-				$this->appendUsage
-			);
-		}
+		if (\is_array($this->appendUsage) && \count($this->appendUsage) > 0)
+			$usage = \array_merge($usage, $this->appendUsage);
 		$usageStr = \implode($usage, ' ');
 		unset($usage);
 		if (!empty($this->msg)) {
 			$msg = Strings::Trim($this->msg);
 			$lines = Strings::WrapLines($msg, self::HELP_WIDTH);
-			foreach ($lines as $line) {
+			foreach ($lines as $line)
 				echo "{$line}\n";
-			}
 		}
-		echo ShellTools::FormatString(
+		echo ShellUtils::FormatString(
 			"{color=orange}Usage:{reset}\n".
 			"  {$usageStr}\n"
 		);
 		echo "\n";
 	}
-	public function Display_Commands() {
-		if (!\is_array($this->commands) || \count($this->commands) == 0) {
+	public function Display_Commands(): void {
+		if (!\is_array($this->commands)
+		|| \count($this->commands) == 0)
 			return;
-		}
-		echo ShellTools::FormatString(
+		echo ShellUtils::FormatString(
 			"{color=orange}Commands:{reset}\n"
 		);
 		// find max command length
 		$maxSize = 0;
 		foreach ($this->commands as $command => $desc) {
 			$len = \mb_strlen($command);
-			if ($len > $maxSize) {
+			if ($len > $maxSize)
 				$maxSize = $len;
-			}
 		}
 		$maxSize += 3;
 		// display commands
@@ -184,7 +149,7 @@ class ShellHelp {
 			$padding = \str_repeat(' ', $maxSize - \mb_strlen($command) );
 			$lines = Strings::WrapLines($desc, self::HELP_WIDTH - $maxSize);
 			$firstLine = $lines[0];
-			echo ShellTools::FormatString(
+			echo ShellUtils::FormatString(
 				"  {color=green}$command{reset}$padding{$firstLine}\n"
 			);
 			// multi-line description
@@ -198,34 +163,24 @@ class ShellHelp {
 		}
 		echo "\n";
 	}
-	public function Display_Flags() {
-		echo ShellTools::FormatString(
+	public function Display_Flags(): void {
+		echo ShellUtils::FormatString(
 			"{color=orange}Flags:{reset}\n"
 		);
-		$firstGroup = (
-			isset($this->flags['pre'])
-			? $this->flags['pre']
-			: []
-		);
-		$lastGroup = (
-			isset($this->flags['pst'])
-			? $this->flags['pst']
-			: []
-		);
+		$firstGroup = (isset($this->flags['pre']) ? $this->flags['pre'] : []);
+		$lastGroup  = (isset($this->flags['pst']) ? $this->flags['pst'] : []);
 		// display flag groups
 		$this->Display_FlagGroup('pre', $firstGroup);
 		foreach ($this->flags as $group => $flags) {
-			if ($group == 'pre' || $group == 'pst') {
+			if ($group == 'pre' || $group == 'pst')
 				continue;
-			}
 			$this->Display_FlagGroup($group, $flags);
 		}
 		$this->Display_FlagGroup('pst', $lastGroup);
 	}
-	public function Display_FlagGroup($group, $flags) {
-		if (!\is_array($flags) || \count($flags) == 0) {
+	public function Display_FlagGroup(string $group, array $flags): void {
+		if (!\is_array($flags) || \count($flags) == 0)
 			return;
-		}
 		$prepared = [];
 		$maxSize = 0;
 		foreach ($flags as $desc => $entries) {
@@ -234,43 +189,33 @@ class ShellHelp {
 			$doubles = [];
 			foreach ($entries as $entry) {
 				$str = Strings::TrimFront($entry, '-');
-				if (empty($str)) {
+				if (empty($str))
 					continue;
-				}
 				if (\mb_strlen($str) == 1) {
 					$singles[] = Strings::ForceStartsWith($entry, '-');
 				} else {
-					while(!\str_starts_with(haystack: $entry, needle: '--')) {
+					while(!\str_starts_with(haystack: $entry, needle: '--'))
 						$entry = "-{$entry}";
-					}
 					$doubles[] = $entry;
 				}
 			}
 			// prepare string "-f, --flag"
-			$flagStr = \implode(
-				\array_merge(
-					$singles,
-					$doubles
-				),
-				', '
-			);
+			$flagStr = \implode(\array_merge($singles, $doubles), ', ');
 			// find max length
 			$len = \mb_strlen($flagStr);
-			if ($len > $maxSize) {
+			if ($len > $maxSize)
 				$maxSize = $len;
-			}
 			$prepared[$desc] = $flagStr;
 		}
-		if ($maxSize < self::MIN_FLAG_SPACING) {
+		if ($maxSize < self::MIN_FLAG_SPACING)
 			$maxSize = self::MIN_FLAG_SPACING;
-		}
 		$maxSize += 3;
 		// display prepared group of flags
 		foreach ($prepared as $desc => $flagStr) {
 			$padding = \str_repeat(' ', $maxSize - \mb_strlen($flagStr) );
 			$lines = Strings::WrapLines($desc, self::HELP_WIDTH - $maxSize);
 			$firstLine = $lines[0];
-			echo ShellTools::FormatString(
+			echo ShellUtils::FormatString(
 				"  {color=green}$flagStr{reset}$padding{$firstLine}\n"
 			);
 			// multi-line description
@@ -288,4 +233,3 @@ class ShellHelp {
 
 
 }
-*/
--- src/utils/ShellUtils.php
+++ src/utils/ShellUtils.php
@@ -5,54 +5,45 @@
  * @license AGPL-3
  * @author lorenzo at poixson.com
  * @link https://poixson.com/
- * /
+ */
 namespace pxn\phpUtils\utils;
 
 
 final class ShellUtils {
 	private final function __construct() {}
 
-	private static $inited = false;
+	private static bool $inited = false;
 
-	private static $flags = null;
-	private static $args  = null;
-	private static $stat  = null;
+	private static array $flags = null;
+	private static array $args  = null;
+	private static array $stat  = null;
 
 
 
 	public static function init() {
-		if (self::$inited) {
+		if (self::$inited)
 			return;
-		}
 		self::initConsoleVars();
 		self::$inited = true;
 		// ansi color enabled
-		if (self::hasFlag('--ansi')) {
+		if (self::hasFlag('--ansi'))
 			ConfigGeneral::setAnsiColorEnabled(true);
-		}
 		// ansi color disabled
-		if (self::hasFlag('--no-ansi')) {
+		if (self::hasFlag('--no-ansi'))
 			ConfigGeneral::setAnsiColorEnabled(false);
-		}
 		// detect color support
 		ConfigGeneral::defaultAnsiColorEnabled(
 			(self::$stat['stdout'] == 'chr')
 		);
 		// clear screen
-		if (ConfigGeneral::isAnsiColorEnabled()) {
+		if (ConfigGeneral::isAnsiColorEnabled())
 			echo self::FormatString('{clear}');
-		}
 	}
-	private static function initConsoleVars() {
-		if (self::$inited) {
-			return;
-		}
-		if (!System::isShell()) {
+	private static function initConsoleVars(): bool {
+		if (self::$inited)      return false;
+		if (!System::isShell()) return false;
+		if (self::$flags !== null || self::$args !== null)
 			return false;
-		}
-		if (self::$flags !== null || self::$args !== null) {
-			return false;
-		}
 		// detect shell state
 		self::$stat = [
 			'stdin'  => self::getStat(\STDIN),
@@ -97,9 +88,8 @@ final class ShellUtils {
 					}
 				}
 				// --flag
-				if (!isset(self::$flags[$arg])) {
+				if (!isset(self::$flags[$arg]))
 					self::$flags[$arg] = true;
-				}
 				continue;
 			}
 			// -flag
@@ -109,9 +99,8 @@ final class ShellUtils {
 				if ($len > 2) {
 					$val = \mb_substr($arg, 2);
 					$arg = \mb_substr($arg, 0, 2);
-					if (\mb_substr($val, 0, 1) == '=') {
+					if (\mb_substr($val, 0, 1) == '=')
 						$val = \mb_substr($val, 1);
-					}
 					self::$flags[$arg] = $val;
 					continue;
 				}
@@ -127,9 +116,8 @@ final class ShellUtils {
 					}
 				}
 				// -f
-				if (!isset(self::$flags[$arg])) {
+				if (!isset(self::$flags[$arg]))
 					self::$flags[$arg] = true;
-				}
 				continue;
 			}
 			// not flag, must be argument
@@ -144,20 +132,13 @@ final class ShellUtils {
 		$stat = \fstat($handle);
 		$mode = $stat['mode'] & 0170000;
 		switch ($mode) {
-		case 0010000:
-			return 'fifo';
-		case 0020000:
-			return 'chr';
-		case 0040000:
-			return 'dir';
-		case 0060000:
-			return 'blk';
-		case 0100000:
-			return 'reg';
-		case 0120000:
-			return 'lnk';
-		case 0140000:
-			return 'sock';
+		case 0010000: return 'fifo';
+		case 0020000: return 'chr';
+		case 0040000: return 'dir';
+		case 0060000: return 'blk';
+		case 0100000: return 'reg';
+		case 0120000: return 'lnk';
+		case 0140000: return 'sock';
 		}
 		return null;
 	}
@@ -175,27 +156,22 @@ final class ShellUtils {
 
 	// get argument (starts at 0)
 	public static function getArg($index=null) {
-		if ($index === null) {
+		if ($index === null)
 			return self::getArg(0);
-		}
 		$index = (int) $index;
-		if (!isset(self::$args[$index])) {
+		if (!isset(self::$args[$index]))
 			return null;
-		}
 		return self::$args[$index];
 	}
 	public static function hasArg($arg) {
-		if (empty($arg)) {
+		if (empty($arg))
 			return null;
-		}
 		// match case
-		if (\in_array($arg, self::$args)) {
+		if (\in_array($arg, self::$args))
 			return true;
-		}
 		// case-insensitive
-		if (\in_array( \strtolower($arg), \array_map('\\strtolower', self::$args) )) {
+		if (\in_array( \strtolower($arg), \array_map('\\strtolower', self::$args) ))
 			return true;
-		}
 		return false;
 	}
 
@@ -203,28 +179,24 @@ final class ShellUtils {
 
 	// get one flag
 	public static function getFlag(... $keys) {
-		if (\count($keys) == 0) {
+		if (\count($keys) == 0)
 			return null;
-		}
 		foreach ($keys as $key) {
 			$val = self::getFlag_Single($key);
-			if ($val !== null) {
+			if ($val !== null)
 				return $val;
-			}
 		}
 		return null;
 	}
 	private static function getFlag_Single($key) {
-		if (empty($key)) {
+		if (empty($key))
 			return null;
-		}
 		if (isset(self::$flags[$key])) {
 			// don't allow "-x value"
 			$AllowShortFlagValues = ConfigGeneral::getAllowShortFlagValues();
 			if (!$AllowShortFlagValues) {
-				if (!\str_starts_with(haystack: $key, needle: '--')) {
+				if (!\str_starts_with(haystack: $key, needle: '--'))
 					return true;
-				}
 			}
 			return self::$flags[$key];
 		}
@@ -235,45 +207,37 @@ final class ShellUtils {
 
 	// get boolean flag
 	public static function getFlagBool(... $keys) {
-		if (\count($keys) == 0) {
+		if (\count($keys) == 0)
 			return null;
-		}
 		foreach ($keys as $key) {
 			$val = self::getFlagBool_Single($key);
-			if ($val !== null) {
+			if ($val !== null)
 				return $val;
-			}
 		}
 		return null;
 	}
 	private static function getFlagBool_Single($key) {
-		if (empty($key)) {
+		if (empty($key))
 			return null;
-		}
-		return General::toBoolean(
-			self::getFlag($key)
-		);
+		return General::toBoolean(self::getFlag($key));
 	}
 
 
 
 	// flag exists
 	public static function hasFlag(... $keys) {
-		if (\count($keys) == 0) {
+		if (\count($keys) == 0)
 			return null;
-		}
 		foreach ($keys as $key) {
 			$result = self::hasFlag_Single($key);
-			if ($result === true) {
+			if ($result === true)
 				return true;
-			}
 		}
 		return false;
 	}
 	private static function hasFlag_Single($key) {
-		if (empty($key)) {
+		if (empty($key))
 			return null;
-		}
 		return isset(self::$flags[$key]);
 	}
 
@@ -302,33 +266,24 @@ final class ShellUtils {
 	}
 	public static function FormatString_Callback(array $matches) {
 		$match = \reset($matches);
-		if (!ConfigGeneral::isAnsiColorEnabled()) {
+		if (!ConfigGeneral::isAnsiColorEnabled())
 			return '';
-		}
 		if (!\str_starts_with(haystack: $match, needle: '{')
-		||  !\str_ends_with(  haystack: $match, needle: '}')) {
+		||  !\str_ends_with(  haystack: $match, needle: '}'))
 			return $match;
-		}
 		$match = \mb_substr($match, 1, -1);
 		$codes = [];
 		$bold = null;
-		$parts = \explode(
-			',',
-			\mb_strtolower($match)
-		);
+		$parts = \explode(',', \mb_strtolower($match));
 		foreach ($parts as $part) {
 			if (empty($part)) continue;
 			$pos = \mb_strpos($part, '=');
 			// {tag}
 			if ($pos === false) {
 				switch ($part) {
-				case 'bold':
-					$bold = true;
-					break;
-				case 'reset':
-					return "\033[0m";
-				case 'clear':
-					return "\033[2J\033[1;1H";
+				case 'bold':  $bold = true; break;
+				case 'reset': return "\033[0m";
+				case 'clear': return "\033[2J\033[1;1H";
 				}
 			// {tag=value}
 			} else {
@@ -337,151 +292,51 @@ final class ShellUtils {
 				if ($key == 'color') {
 					switch ($val) {
 					// dark colors
-					case 'black':
-						$codes[] = 30;
-						$bold = ($bold === null ? false : $bold);
-						break;
-					case 'red':
-						$codes[] = 31;
-						$bold = ($bold === null ? false : $bold);
-						break;
-					case 'green':
-						$codes[] = 32;
-						$bold = ($bold === null ? false : $bold);
-						break;
-					case 'orange':
-						$codes[] = 33;
-						$bold = ($bold === null ? false : $bold);
-						break;
-					case 'blue':
-						$codes[] = 34;
-						$bold = ($bold === null ? false : $bold);
-						break;
-					case 'magenta':
-						$codes[] = 35;
-						$bold = ($bold === null ? false : $bold);
-						break;
-					case 'cyan':
-						$codes[] = 36;
-						$bold = ($bold === null ? false : $bold);
-						break;
-					case 'lightgray':
-						$codes[] = 37;
-						$bold = ($bold === null ? false : $bold);
-						break;
+					case 'black':     $codes[] = 30; $bold = ($bold === null ? false : $bold); break;
+					case 'red':       $codes[] = 31; $bold = ($bold === null ? false : $bold); break;
+					case 'green':     $codes[] = 32; $bold = ($bold === null ? false : $bold); break;
+					case 'orange':    $codes[] = 33; $bold = ($bold === null ? false : $bold); break;
+					case 'blue':      $codes[] = 34; $bold = ($bold === null ? false : $bold); break;
+					case 'magenta':   $codes[] = 35; $bold = ($bold === null ? false : $bold); break;
+					case 'cyan':      $codes[] = 36; $bold = ($bold === null ? false : $bold); break;
+					case 'lightgray': $codes[] = 37; $bold = ($bold === null ? false : $bold); break;
 					// light colors
-					case 'gray':
-						$codes[] = 30;
-						$bold = ($bold === null ? true : $bold);
-						break;
-					case 'lightred':
-						$codes[] = 31;
-						$bold = ($bold === null ? true : $bold);
-						break;
-					case 'lime':
-						$codes[] = 32;
-						$bold = ($bold === null ? true : $bold);
-						break;
-					case 'yellow':
-						$codes[] = 33;
-						$bold = ($bold === null ? true : $bold);
-						break;
-					case 'lightblue':
-						$codes[] = 34;
-						$bold = ($bold === null ? true : $bold);
-						break;
-					case 'pink':
-						$codes[] = 35;
-						$bold = ($bold === null ? true : $bold);
-						break;
-					case 'lightcyan':
-						$codes[] = 36;
-						$bold = ($bold === null ? true : $bold);
-						break;
-					case 'white':
-						$codes[] = 37;
-						$bold = ($bold === null ? true : $bold);
-						break;
+					case 'gray':      $codes[] = 30; $bold = ($bold === null ? true : $bold); break;
+					case 'lightred':  $codes[] = 31; $bold = ($bold === null ? true : $bold); break;
+					case 'lime':      $codes[] = 32; $bold = ($bold === null ? true : $bold); break;
+					case 'yellow':    $codes[] = 33; $bold = ($bold === null ? true : $bold); break;
+					case 'lightblue': $codes[] = 34; $bold = ($bold === null ? true : $bold); break;
+					case 'pink':      $codes[] = 35; $bold = ($bold === null ? true : $bold); break;
+					case 'lightcyan': $codes[] = 36; $bold = ($bold === null ? true : $bold); break;
+					case 'white':     $codes[] = 37; $bold = ($bold === null ? true : $bold); break;
 					}
 				} else // end color tag
 				if ($key == 'bg' || $key == 'bgcolor' || $key == 'back') {
 					switch ($val) {
 					// dark colors
-					case 'black':
-						$codes[] = 40;
-						$bold = ($bold === null ? false : $bold);
-						break;
-					case 'red':
-						$codes[] = 41;
-						$bold = ($bold === null ? false : $bold);
-						break;
-					case 'green':
-						$codes[] = 42;
-						$bold = ($bold === null ? false : $bold);
-						break;
-					case 'orange':
-						$codes[] = 43;
-						$bold = ($bold === null ? false : $bold);
-						break;
-					case 'blue':
-						$codes[] = 44;
-						$bold = ($bold === null ? false : $bold);
-						break;
-					case 'magenta':
-						$codes[] = 45;
-						$bold = ($bold === null ? false : $bold);
-						break;
-					case 'cyan':
-						$codes[] = 46;
-						$bold = ($bold === null ? false : $bold);
-						break;
-					case 'lightgray':
-						$codes[] = 47;
-						$bold = ($bold === null ? false : $bold);
-						break;
+					case 'black':     $codes[] = 40; $bold = ($bold === null ? false : $bold); break;
+					case 'red':       $codes[] = 41; $bold = ($bold === null ? false : $bold); break;
+					case 'green':     $codes[] = 42; $bold = ($bold === null ? false : $bold); break;
+					case 'orange':    $codes[] = 43; $bold = ($bold === null ? false : $bold); break;
+					case 'blue':      $codes[] = 44; $bold = ($bold === null ? false : $bold); break;
+					case 'magenta':   $codes[] = 45; $bold = ($bold === null ? false : $bold); break;
+					case 'cyan':      $codes[] = 46; $bold = ($bold === null ? false : $bold); break;
+					case 'lightgray': $codes[] = 47; $bold = ($bold === null ? false : $bold); break;
 					// light colors
-					case 'gray':
-						$codes[] = 40;
-						$bold = ($bold === null ? true : $bold);
-						break;
-					case 'lightred':
-						$codes[] = 41;
-						$bold = ($bold === null ? true : $bold);
-						break;
-					case 'lime':
-						$codes[] = 42;
-						$bold = ($bold === null ? true : $bold);
-						break;
-					case 'yellow':
-						$codes[] = 43;
-						$bold = ($bold === null ? true : $bold);
-						break;
-					case 'lightblue':
-						$codes[] = 44;
-						$bold = ($bold === null ? true : $bold);
-						break;
-					case 'pink':
-						$codes[] = 45;
-						$bold = ($bold === null ? true : $bold);
-						break;
-					case 'lightcyan':
-						$codes[] = 46;
-						$bold = ($bold === null ? true : $bold);
-						break;
-					case 'white':
-						$codes[] = 47;
-						$bold = ($bold === null ? true : $bold);
-						break;
+					case 'gray':      $codes[] = 40; $bold = ($bold === null ? true : $bold); break;
+					case 'lightred':  $codes[] = 41; $bold = ($bold === null ? true : $bold); break;
+					case 'lime':      $codes[] = 42; $bold = ($bold === null ? true : $bold); break;
+					case 'yellow':    $codes[] = 43; $bold = ($bold === null ? true : $bold); break;
+					case 'lightblue': $codes[] = 44; $bold = ($bold === null ? true : $bold); break;
+					case 'pink':      $codes[] = 45; $bold = ($bold === null ? true : $bold); break;
+					case 'lightcyan': $codes[] = 46; $bold = ($bold === null ? true : $bold); break;
+					case 'white':     $codes[] = 47; $bold = ($bold === null ? true : $bold); break;
 					}
 				} // end bgcolor tag
 			} // end {tag=value}
 		} // end for
-		if ($bold !== null) {
-			\array_unshift(
-				$codes,
-				($bold !== false ? 1 : 0)
-			);
-		}
+		if ($bold !== null)
+			\array_unshift($codes, ($bold !== false ? 1 : 0));
 		if (\count($codes) > 0) {
 			$code = \implode(';', $codes);
 			return "\033[{$code}m";
@@ -492,4 +347,3 @@ final class ShellUtils {
 
 
 }
-*/
